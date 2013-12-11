a6d34e54f849d06979c53f3fe966d1b39b39ef14
David Lin <linwdav@gmail.com>
2013-12-10 23:57:14 -0800

    Add basic tip calculator functionality

---
+++ tipcalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tipcalculator
+//
+//  Created by David Lin on 12/10/13.
+//  Copyright (c) 2013 David Lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ tipcalculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  tipcalculator
+//
+//  Created by David Lin on 12/10/13.
+//  Copyright (c) 2013 David Lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
